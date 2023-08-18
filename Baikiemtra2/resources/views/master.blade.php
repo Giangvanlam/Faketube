b734b894fc9e4a732ec0389c603449540c2dee58
Giang Văn Lâm <van131361@gmain.com>
2023-08-18 17:04:48 +0700

    Push

--- Baikiemtra2/resources/views/master.blade.php
+++ Baikiemtra2/resources/views/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Motorcycle Management</title>
+    <title>Channel Management</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <style>
